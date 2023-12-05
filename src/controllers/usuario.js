4b8d8624c4727843b85d8cf0b1999e83e9540076
JhonyHC <jonhatan.higuera5180@alumnos.udg.mx>
2023-12-04 21:58:06 -0600

    Valida que no exista el usuario

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,13 +1,22 @@
-const { agregarUsuario } = require("../services/usuario");
-
+const { agregarUsuario, encontrarPorUsername, findOneByEmail } = require("../services/usuario");
 
 exports.crearUsuario = async function (request, response) {
 	const { email, username, password } = request.body;
-	const user = await agregarUsuario({ email, username, password });
-	response.status(201).json(user);
+	//Check if user exists
+	const usernameExists = await encontrarPorUsername(username);
+	if (usernameExists) {
+	  return response.status(400).json({ message: "El usuario ya existe" });
+	}
+	const emailExists = await findOneByEmail(email);
+	if (emailExists) {
+	  return response.status(400).json({ message: "Esta email ya está en uso" });
+	}
+
+  const user = await agregarUsuario({ email, username, password });
+  response.status(201).json(user);
 };
 
 exports.me = async function (request, response) {
-	const {username, email} = request.user;
-	response.status(200).json({username, email});
+  const { username, email } = request.user;
+  response.status(200).json({ username, email });
 };
